a02d222b02d0a9bbc81b47ea512c4a85586e1c76
Akshay-Syscort <akshay.shaharwale@syscort.com>
2024-11-19 13:48:21 +0530

    third

---
+++ upload/upload/doctype/git_2/git_2.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, akshay.shaharwale@syscort.com and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("git 2", {
+// 	refresh(frm) {
+
+// 	},
+// });
